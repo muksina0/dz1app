03799906b5f9ea66191440c9926558eef39ce906
Muksina <abdudujs1@gmail.com>
2024-11-21 15:15:29 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
- // var name = "musina"
- // console.log(name)
- //  var info =2008;
- // console.log(info)
+var  grade = Number(prompt('Введите оценку от 1 до 10').trim());
+
+if (grade >= 5 && grade <= 10 ) {
+    alert('Cпасибо за высокую оценку!')
+}else if (grade < 5) {
+    alert('Мы работаем над улучшением сервиса')
+}else {
+
+    alert('Введите оценку от 1 до 10')
+}
   
